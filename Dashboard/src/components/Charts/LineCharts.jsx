a0eeddd5ca2822250cc082b81607f9af6a0b735b
Alexandre Amado <alexandreamado71@gmail.com>
2024-02-02 14:02:15 +0000

    building a dashboard

---
+++ Dashboard/src/components/Charts/LineCharts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LineCharts = () => {
+  return (
+    <div>
+      <div>Line</div>
+    </div>
+  )
+}
+
+export default LineCharts
---
+++ Dashboard/src/components/Charts/SparkLine.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SparkLine = () => {
+  return (
+    <div>
+      SparklLine
+    </div>
+  )
+}
+
+export default SparkLine
